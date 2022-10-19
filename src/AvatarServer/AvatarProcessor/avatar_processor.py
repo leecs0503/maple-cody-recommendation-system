1e95c0154dd51c2c3393bf8c2c346d29283eb602
lee changsu <leecs0503@naver.com>
2022-10-19 17:06:08 +0900

    tests(avatar_processor): 테스트 코드 구현

--- src/AvatarServer/AvatarProcessor/avatar_processor.py
+++ src/AvatarServer/AvatarProcessor/avatar_processor.py
@@ -9,6 +9,43 @@ from .WCR_caller import WCRCaller
 from PIL import Image
 import base64
 import io
+import dataclasses
+
+
+@dataclasses.dataclass
+class PackedCharacterInfo:
+    gender: int = -1
+    skin_id: int = -1
+    face_id: int = -1
+    face_gender: int = -1
+    is_hair_over_40000: int = -1
+    hair_id: int = -1
+    hair_gender: int = -1
+    cap_id: int = -1
+    cap_gender: int = -1
+    face_accessory_id: int = -1
+    face_accessory_gender: int = -1
+    eye_accessory_id: int = -1
+    eye_accessory_gender: int = -1
+    ear_accessory_id: int = -1
+    ear_accessory_gender: int = -1
+    is_long_coat: int = -1
+    coat_id: int = -1
+    coat_gender: int = -1
+    pants_id: int = -1
+    pants_gender: int = -1
+    shoes_id: int = -1
+    shoes_gender: int = -1
+    glove_id: int = -1
+    glove_gender: int = -1
+    cape_id: int = -1
+    cape_gender: int = -1
+    shield_id: int = -1
+    shield_gender: int = -1
+    weapon_id: int = -1
+    weapon_gender: int = -1
+    hair_mix_color: int = -1
+    hair_mix_ratio: int = -1
 
 
 class AvatarProcessor:
@@ -16,10 +53,11 @@ class AvatarProcessor:
         self,
         logger: logging.Logger,
         config: Config,
+        caller: WCRCaller = None,
     ):
         self.logger = logger
         self.base_wz_code_path = config.base_wz_code_path
-        self.caller = WCRCaller(
+        self.caller = caller if caller is not None else WCRCaller(
             logger=self.logger,
             wcr_server_host=config.wcr_server_host,
             wcr_server_protocol=config.wcr_server_protocol,
@@ -68,4 +106,4 @@ class AvatarProcessor:
 
     def infer(self, packed_character_look: str) -> Avatar:
         # TODO: implement
-        return Avatar()
+        return PackedCharacterInfo()
--- tests/test_avatar_server/test_avatar_processor/test_avatar_processor.py
+++ tests/test_avatar_server/test_avatar_processor/test_avatar_processor.py
@@ -1,5 +1,72 @@
-# TODO: implement
+from src.AvatarServer.util.item_manager import ItemManager
+from src.AvatarServer.AvatarProcessor.avatar_processor import AvatarProcessor, PackedCharacterInfo
+from src.AvatarServer.AvatarProcessor.WCR_caller import WCRCaller
+from src.AvatarServer.server.config import Config
+from src.AvatarServer.Avatar.avatar import Avatar
+from typing import Optional
+import json
+import pytest
+import logging
+import os
 
+
+class CallerForTest(WCRCaller):
+    def __init__(*args, **kwargs):
+        pass
+
+    async def get_base_wz(self):
+        return {}
+
+    async def get_image(self, avatar: Avatar, ActionQuery: Optional[str] = None):
+        return None
+
+
+@pytest.fixture
+def caller_for_test() -> CallerForTest:
+    return CallerForTest()
+
+
+@pytest.fixture
+def avatar_processor_for_test(config_for_test: Config, caller_for_test: CallerForTest) -> AvatarProcessor:
+
+    # FIXME: validate 로직 제거 예정
+    async def tmp(*args, **kwargs):
+        pass
+    ItemManager.validate = tmp
+    return AvatarProcessor(
+        logger=logging.getLogger(__file__),
+        config=config_for_test,
+        caller=caller_for_test,
+    )
+
+
+def test_infer(avatar_processor_for_test: AvatarProcessor):
+    cwd = os.path.dirname(__file__)
+    json_path = os.path.join(cwd, 'testdata', 'packed_info.json')
+
+    with open(json_path, 'r') as file:
+        data: dict = json.load(file)
+    for user_name, user_data in data.items():
+        expected_return_data = PackedCharacterInfo()
+        for k, v in user_data.items():
+            if k == "str":
+                continue
+            setattr(expected_return_data, k, v)
+
+        packed_character_look = user_data["str"]
+        return_data = avatar_processor_for_test.infer(packed_character_look=packed_character_look)
+        for k, v in user_data.items():
+            if k == "str":
+                continue
+            v1 = getattr(return_data, k)
+            v2 = getattr(expected_return_data, k)
+            if v1 != v2:
+                print(f"!! {user_name}: {k} is different ({v1} <-> {v2}).")
+
+        assert expected_return_data == return_data
+
+
+@pytest.mark.asyncio
 def test_process_image():
-
+    # TODO: implement
     pass
