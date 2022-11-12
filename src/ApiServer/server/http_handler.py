e405caba84e739cbf5d5adb7882334b77a62e330
Yu KyungHwan <dbrudghks1234@gmail.com>
2022-11-12 23:38:31 +0900

    feat: webserver image 코드 추가

--- src/ApiServer/server/http_handler.py
+++ src/ApiServer/server/http_handler.py
@@ -6,6 +6,7 @@ from aiohttp import web
 from ..server.config import Config
 import requests
 from bs4 import BeautifulSoup
+import json
 
 
 def get_html_text(url: str):
@@ -26,7 +27,8 @@ class HTTPHandler:
         return [
             web.get("/", self.index_handler),
             web.get("/healthcheck", self.healthcheck_handler),
-            web.post('/web_handler', self.web_handler)
+            web.post('/web_handler', self.web_handler),
+            web.post('/infer_handler', self.infer_handler),
         ]
 
     async def index_handler(self, request: web.Request):
@@ -56,3 +58,28 @@ class HTTPHandler:
         print(response.text)
 
         return web.Response(body=response.text, status=HTTPStatus.OK)
+
+    async def infer_handler(self, request: web.Request):
+
+        post = await request.text()
+        # response = requests.post("http://localhost:8080/inference", post)  -> infer 서버에 사용자의 코드 요청
+        # 추천된 코드를 json으로 저장하여 response로 받는다고 가정
+
+        response = '{"face": "54002", "cap": "1005041", "longcoat": "1053240", "weapon": "1703048", "cape": "1103332", "coat": "0", "glove": "1082703", "hair": "61481+3*50", "pants": "0", "shield": "1092067", "shoes": "1073534", "faceAccessory": "1012050", "eyeAccessory": "1022079", "earrings": "1032022", "skin": "12024"}'
+        response = json.loads(response)
+
+        obj = {}
+        for k, v in response.items():
+            if k == 'skin':
+                k = 'head'
+            if k == 'shield':
+                continue
+            if v == '0':
+                continue
+            if k == 'name':
+                continue
+            obj[k] = v
+        obj['bs'] = 'true'
+        res = requests.get('https://0.0.0.0:7209/avatar', params=obj, verify=False)
+
+        return web.Response(body=res.text, status=HTTPStatus.OK)
--- src/WebServer/server.js
+++ src/WebServer/server.js
@@ -8,45 +8,59 @@ const { create } = require('domain');
 
 
 
+
 app.set("view engine", "html")
 nunjucks.configure("./views", {
-    express: app
+  express: app
 });
 
 
-const server = app.listen(3000, () =>{
-    console.log("Start Server : localhost: 3000");
+const server = app.listen(3000, () => {
+  console.log("Start Server : localhost: 3000");
 }
 );
 
 
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
-    let name = req.query.data
-    res.render('index.html',{
-        user: name
-    })
-  });
+  let name = req.query.data
+  res.render('index.html', {
+    user: name
+  })
+});
 
 var result
-app.post("/result", async(req, res)=>{
+var infer_result
+app.post("/result", async (req, res) => {
 
-    await axios.post('http://localhost:7000/web_handler', {
-        name: req.body.name
-      })
-      .then(function (response) {
-        result = response.data;
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-      result = JSON.stringify(result)
-      console.log(result)
-      res.render('result.html',{
-        data: result
+  await axios.post('http://localhost:7000/web_handler', {
+    name: req.body.name
+  })
+    .then(function (response) {
+      result = response.data;
     })
+    .catch(function (error) {
+      console.log(error);
+    });
+  result = JSON.stringify(result)
+  json = JSON.parse(result)
+
+  await axios.post('http://localhost:7000/infer_handler', {
+    result
+  })
+    .then(function (response) {
+      infer_result = response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  await res.render('result.html', {
+    name: req.body.name,
+    decoded_image: infer_result
+  })
 
 });
