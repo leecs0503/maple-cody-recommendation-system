bc0a9586e1e2f117eb35f56b2fda4f5dfc9f5234
kig9981 <ingu9981@naver.com>
2022-11-21 16:35:45 +0900

    fix(make_train_data_prototype): 100명마다 끊어서 json파일을 추가로 출력하도록 변경

--- notebook/make_train_data_prototype/script.py
+++ notebook/make_train_data_prototype/script.py
@@ -23,15 +23,26 @@ with open(path3) as file:
 
 # %%
 result = {}
+cresult = {}
+cnt = 1
 
 for json_data in [json_data1, json_data2, json_data3]:
     for data in json_data['data']:
         result[data["nickname"]] = []
+        cresult[data["nickname"]] = []
         for code_list in data["recent_cody_list"]:
             encrypted_code = code_list.replace('https://avatar.maplestory.nexon.com/Character/', '').replace('.png', '')
             response = requests.post("http://localhost:8080/packed_character_look", json={"packed_character_look": encrypted_code})  # noqa: E501
             if response.status_code == 200:
                 result[data["nickname"]].append(json.loads(response.text))
+                cresult[data["nickname"]].append(json.loads(response.text))
+        if len(cresult) == 100:
+            save_path = os.path.join(file_path, f'json_data_result{cnt}.json')
+            cnt += 1
+
+            with open(save_path, "w") as f:
+                json.dump(cresult, f, ensure_ascii=False, indent="\t")            
+            cresult = {}
 
 save_path = os.path.join(file_path, 'json_data_result_1_3000.json')
 
