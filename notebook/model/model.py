68ea815cb2c5c4b24c069c8a397e32ecbf74291b
changsu lee <leecs0503@naver.com>
2022-11-23 13:04:08 +0900

    중간 커밋

--- notebook/model/model.py
+++ notebook/model/model.py
@@ -1,4 +1,4 @@
-from torchvision.models import resnet18
+from torchvision.models import resnet50
 from torch import nn
 import torch
 
@@ -8,27 +8,31 @@ class Model(nn.Module):
         num_result_classes,
     ):
         super().__init__()
-        image_model = resnet18(pretrained=False)
+        image_model = resnet50(pretrained=True)
+        for param in image_model.parameters():
+            param.requires_grad = False
         num_ftrs = image_model.fc.in_features # 1000
         image_model.fc = nn.Linear(num_ftrs, num_ftrs) 
         self.image_model = image_model
-        self.fc1 = nn.Linear(num_ftrs, num_result_classes)
-        self.fc2 = nn.Linear(num_result_classes, num_result_classes)
+        self.fc1 = nn.Linear(num_ftrs, num_ftrs)
+        self.fc2 = nn.Linear(num_ftrs, num_result_classes)
         self.dropout = nn.Dropout(0.5)
         self.sigmoid = torch.nn.Sigmoid()
+        self.relu = torch.nn.ReLU()
+        self.softmax = torch.nn.Softmax(dim=1)
 
     def forward(
         self,
         image_tensor,
     ):
         result_feature = self.image_model(image_tensor)
-        
         l2_norm = result_feature.norm(p=2, dim=1, keepdim=True).detach()
         result_feature = result_feature.div(l2_norm) # l2-normalized feature vector
-        result_feature = self.sigmoid(result_feature)
+        result_feature = self.relu(result_feature)
         result_feature = self.dropout(result_feature)
         result_feature = self.fc1(result_feature)
-        result_feature = self.sigmoid(result_feature)
+        result_feature = self.relu(result_feature)
         result_feature = self.dropout(result_feature)
         result_feature = self.fc2(result_feature)
+        # result_feature = self.sigmoid(result_feature)
         return result_feature
--- notebook/model/train.py
+++ notebook/model/train.py
@@ -15,13 +15,20 @@ from io import BytesIO
 import torchvision.transforms as transforms
 from typing import List
 random.seed(42)
-with open('./json_data_result1.json', 'r') as f:
+device = torch.device("cuda")
+with open('./json_data_result_1_3000.json', 'r') as f:
     raw_data: dict = json.load(f)
 
 input_list: List[torch.Tensor] = []
+weapon_set = set()
 expected_output_list = []
 expected_output_tensors = []
 cap_set = set()
+datas = {}
+def add_datas(K, V):
+    if K not in datas:
+        datas[K] = []
+    datas[K].append(V)
 
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -30,49 +37,65 @@ transform = transforms.Compose([
 for crt_name, avatar_info_list in raw_data.items():
     for avatar_info in avatar_info_list:
         weapon_image_b64 = avatar_info["weapon_image"]
+        add_datas(avatar_info["weapon"], avatar_info["cap"])
         if weapon_image_b64 == "":
             continue
-        weapon_image = Image.open(BytesIO(base64.b64decode(weapon_image_b64)))
-        weapon_image = weapon_image.convert("RGB")
-        weapon_image = weapon_image.resize((224,224), Image.ANTIALIAS)
-        input_list.append(transform(weapon_image))
-        expected_output_list.append(avatar_info["cap"])
+
+        weapon_set.add(
+            (avatar_info["weapon"], weapon_image_b64)
+        )
         cap_set.add(avatar_info["cap"])
 inv_dict = {}
 for idx, val in enumerate(cap_set):
     inv_dict[val] = idx
 cap_num = len(cap_set)
 expected_output_tensors = []
-for output in expected_output_list:
-    x = torch.zeros(cap_num)
-    x[inv_dict[output]] = 1
-    expected_output_tensors.append(x)
+
+for (weapon, weapon_image_b64) in weapon_set:
+    weapon_image = Image.open(BytesIO(base64.b64decode(weapon_image_b64)))
+    weapon_image = weapon_image.convert("RGB")
+    result = weapon_image.resize((224, 224), Image.ANTIALIAS)
+    # result = Image.new(weapon_image.mode, (224, 224), (0, 0, 0))
+    # result.paste(weapon_image, (0, 0))
+    # result.show()
+    L = datas[weapon]
+    output = torch.zeros(cap_num)
+    for cap in L:
+        output[inv_dict[cap]] += 1
+    output = output.div(torch.sum(output))
+    input_list.append(transform(result))
+    expected_output_tensors.append(output)
+print(len(input_list))
+# for output in expected_output_list:
+#     # x = torch.zeros(cap_num, dtype=torch.long).to(device)
+#     # x[inv_dict[output]] = 1
+#     expected_output_tensors.append(torch.tensor([inv_dict[output]]))
 
 # %%
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-epoch_num = 200
-learning_rate = 0.002
+epoch_num = 1000
+learning_rate = 0.001
 gamma = 0.1
-step_size = 10
+step_size = 20
 
 
 model = Model(
     num_result_classes=cap_num,
-)
+).to(device)
 params = model.parameters()
 
 optimizer = optim.Adam(params, lr=learning_rate)
+# scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
 scheduler = lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
-# criterion = nn.BCELoss(reduction='sum').to(device)
-criterion =  nn.CrossEntropyLoss().to(device)
+# criterion = nn.BCELoss(reduction="sum").to(device)
+criterion =  nn.MSELoss(reduction="sum").to(device)
 
 batch_size = 32
 for epoch in range(0, epoch_num):
     running_loss = 0.0
     L = [
-        (input, answer, eo)
-        for input, answer, eo in zip(input_list, expected_output_tensors, expected_output_list)
+        (input, answer)
+        for input, answer in zip(input_list, expected_output_tensors)
     ]
     random.shuffle(L)
     run_cnt = 0
@@ -81,24 +104,23 @@ for epoch in range(0, epoch_num):
     step_num = 0
     for idx in range(0, len(L), batch_size):
         R = L[idx:idx+batch_size]
+        bs = len(R)
         inputs = []
         answers = []
-        answer_v = []
-        for input, answer, eo in R:
+        for input, answer in R:
             inputs.append(input)
             answers.append(answer)
-            answer_v.append(eo)
 
-        input_tensor = torch.stack(inputs)
-        answer_tensor = torch.stack(answers)
+        input_tensor = torch.stack(inputs).to(device)
+        answer_tensor = torch.stack(answers).to(device)
         output = model(input_tensor)
         loss = criterion(output, answer_tensor)
         optimizer.zero_grad()
         running_loss += loss.item()
         _, o_v = torch.max(output, 1)
-        for o, eo in zip(o_v, answer_v):
-            eo = inv_dict[eo]
-            if o.item() == eo:
+        qqq = 0
+        for o, answer_tensor in zip(o_v, answers):
+            if not -1e-9 <= answer_tensor[o.item()] <= 1e-9:
                 accept_cnt += 1
             all_cnt += 1
 
@@ -106,7 +128,9 @@ for epoch in range(0, epoch_num):
         optimizer.step()
         run_cnt += 1
         step_num += 1
-        print(f"EPOCH [{epoch:3d}] + STEP [{step_num:3d}] loss: {running_loss / run_cnt:.5f} // acc: {accept_cnt / all_cnt:.5f}")
-    print(f"EPOCH [{epoch:3d}]: loss: {running_loss / run_cnt:.5f} // acc: {accept_cnt / all_cnt:.5f}")
+        # if step_num % 10 == 0:
+        #     print(f"EPOCH [{epoch:3d}] + STEP [{step_num:3d}] loss: {running_loss / run_cnt:.5f} // acc: {accept_cnt / all_cnt:.5f}")
+    print(f"EPOCH [{epoch:3d}]: loss: {running_loss / all_cnt:.5f} // acc: {accept_cnt / all_cnt:.5f}")
+    scheduler.step()
 
 torch.save(model.state_dict(), './model_result.pth')
