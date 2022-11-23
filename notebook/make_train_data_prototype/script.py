8433ee8af81a3f62868e50f86b8ef47559c2321d
kig9981 <ingu9981@naver.com>
2022-11-23 15:30:54 +0900

    fix(make_train_data_prototype): 100개 단위로 데이터 수집 후 마지막에 합치도록 수정

--- notebook/make_train_data_prototype/script.py
+++ notebook/make_train_data_prototype/script.py
@@ -22,30 +22,46 @@ with open(path3) as file:
     json_data3 = json.load(file)
 
 # %%
-result = {}
-cresult = {}
+
+import requests
+
 cnt = 1
+result = {}
 
 for json_data in [json_data1, json_data2, json_data3]:
     for data in json_data['data']:
         result[data["nickname"]] = []
-        cresult[data["nickname"]] = []
         for code_list in data["recent_cody_list"]:
             encrypted_code = code_list.replace('https://avatar.maplestory.nexon.com/Character/', '').replace('.png', '')
-            response = requests.post("http://localhost:8080/packed_character_look", json={"packed_character_look": encrypted_code})  # noqa: E501
+            response = requests.post("http://localhost:8080/character_look_data", json={"packed_character_look": encrypted_code})
             if response.status_code == 200:
                 result[data["nickname"]].append(json.loads(response.text))
-                cresult[data["nickname"]].append(json.loads(response.text))
-        if len(cresult) == 100:
+        if len(result) == 100:
             save_path = os.path.join(file_path, f'json_data_result{cnt}.json')
             cnt += 1
 
             with open(save_path, "w") as f:
-                json.dump(cresult, f, ensure_ascii=False, indent="\t")            
-            cresult = {}
+                json.dump(result, f, ensure_ascii=False, indent="\t")
 
+if len(result) > 0:
+    save_path = os.path.join(file_path, f'json_data_result{cnt}.json')
+
+    with open(save_path, "w") as f:
+        json.dump(result, f, ensure_ascii=False, indent="\t")
+
+result = {}
 save_path = os.path.join(file_path, 'json_data_result_1_3000.json')
+
+for i in range(1,31):
+    load_path = os.path.join(file_path, f'json_data_result{i}.json')
+
+    with open(load_path, "r") as f:
+        temp = json.load(f)
+        for k, v in temp.items():
+            result[k] = v
 
 with open(save_path, "w") as f:
     json.dump(result, f, ensure_ascii=False, indent="\t")
+
+
 # %%
