b403aa109674c1a71fe66fce8ab76a28a6496d31
kig9981 <ingu9981@naver.com>
2022-11-29 11:40:45 +0900

    make_train_data_prototype/script.py 수정

--- notebook/make_train_data_prototype/script.py
+++ notebook/make_train_data_prototype/script.py
@@ -8,18 +8,19 @@ file_path = os.path.abspath(os.path.join(cwd, os.pardir))
 file_path = os.path.abspath(os.path.join(file_path, os.pardir))
 file_path = os.path.join(file_path, 'data')
 
-path1 = os.path.join(file_path, 'json_data_1_1000.json')
-path2 = os.path.join(file_path, 'json_data_1001_2000.json')
-path3 = os.path.join(file_path, 'json_data_2001_3000.json')
+path1 = os.path.join(file_path, 'new_json_data_1_3000.json')
+path2 = os.path.join(file_path, 'new_json_data_3001_10000.json')
+path3 = os.path.join(file_path, 'new_json_data_10001_15000.json')
+path4 = os.path.join(file_path, 'new_json_data_15001_20000.json')
+path5 = os.path.join(file_path, 'new_json_data_20001_25000.json')
 
-with open(path1) as file:
-    json_data1 = json.load(file)
+paths = [path1, path2, path3, path4, path5]
+json_datas = []
 
-with open(path2) as file:
-    json_data2 = json.load(file)
+for path in paths:
+    with open(path) as file:
+        json_datas.append(json.load(file))
 
-with open(path3) as file:
-    json_data3 = json.load(file)
 
 # %%
 
@@ -28,7 +29,7 @@ import requests
 cnt = 1
 result = {}
 
-for json_data in [json_data1, json_data2, json_data3]:
+for json_data in json_datas:
     for data in json_data['data']:
         result[data["nickname"]] = []
         for code_list in data["recent_cody_list"]:
@@ -36,6 +37,8 @@ for json_data in [json_data1, json_data2, json_data3]:
             response = requests.post("http://localhost:8080/character_look_data", json={"packed_character_look": encrypted_code})
             if response.status_code == 200:
                 result[data["nickname"]].append(json.loads(response.text))
+            else:
+                print(response.status_code)
         if len(result) == 100:
             save_path = os.path.join(file_path, f'json_data_result{cnt}.json')
             cnt += 1
@@ -48,13 +51,15 @@ for json_data in [json_data1, json_data2, json_data3]:
 if len(result) > 0:
     save_path = os.path.join(file_path, f'json_data_result{cnt}.json')
 
+    cnt += 1
+
     with open(save_path, "w") as f:
         json.dump(result, f, ensure_ascii=False, indent="\t")
 
 result = {}
-save_path = os.path.join(file_path, 'json_data_result_1_3000.json')
+save_path = os.path.join(file_path, 'json_data_result_1_25000.json')
 
-for i in range(1,31):
+for i in range(1, cnt):
     load_path = os.path.join(file_path, f'json_data_result{i}.json')
 
     with open(load_path, "r") as f:
