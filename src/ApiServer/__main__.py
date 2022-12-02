92d99d6b5106c5416be3339e8229737410795c95
kig9981 <ingu9981@naver.com>
2022-12-02 18:16:55 +0900

    기존 config 추가

--- src/ApiServer/__main__.py
+++ src/ApiServer/__main__.py
@@ -8,6 +8,14 @@ def main():
 
     parser.add_argument("--logging-path", type=str, help="api server loggin path", default='logs/api_server_log.log')
 
+    parser.add_argument("--wcr-server-host", type=str, help="wcr server host", default="localhost")
+    parser.add_argument("--wcr-server-port", type=int, help="wcr server port", default=7209)
+    parser.add_argument("--wcr-server-protocol", type=str, help="wcr server protocol", default="https")
+    parser.add_argument("--base-wz-code-path", type=str, help="base", default="./data/base_wz.json")
+    parser.add_argument("--wcr-caller-retry-num", type=int, help="wcr server retry num", default=-1)
+    parser.add_argument("--wcr-caller-timeout", type=float, help="wcr server caller timeout", default=2.5)
+    parser.add_argument("--wcr-caller-backoff", type=float, help="wcr server caller backoff", default=1)
+
     parser.add_argument("--avatar-server-host", type=str, help="avatar server host", default="0.0.0.0")
     parser.add_argument("--avatar-server-protocol", type=str, help="avatar server protocol", default="http")
     parser.add_argument("--avatar-server-port", type=int, help="avatar server port", default=8080)
@@ -24,6 +32,13 @@ def main():
     args = parser.parse_args()
 
     config = Config(
+        wcr_server_host=args.wcr_server_host,
+        wcr_server_port=args.wcr_server_port,
+        wcr_server_protocol=args.wcr_server_protocol,
+        base_wz_code_path=args.base_wz_code_path,
+        wcr_caller_retry_num=args.wcr_caller_retry_num,
+        wcr_caller_timeout=args.wcr_caller_timeout,
+        wcr_caller_backoff=args.wcr_caller_backoff,
         avatar_server_host=args.avatar_server_host,
         avatar_server_protocol=args.avatar_server_protocol,
         avatar_server_port=args.avatar_server_port,
--- src/ApiServer/server/config.py
+++ src/ApiServer/server/config.py
@@ -1,6 +1,13 @@
 class Config:
     def __init__(
         self,
+        wcr_server_host: str,
+        wcr_server_port: int,
+        wcr_server_protocol: str,
+        base_wz_code_path: str,
+        wcr_caller_retry_num: int,
+        wcr_caller_timeout: float,
+        wcr_caller_backoff: float,
         avatar_server_host: str,
         avatar_server_protocol: str,
         avatar_server_port: int,
@@ -14,6 +21,13 @@ class Config:
         inference_caller_timeout: float,
         inference_caller_backoff: float,
     ) -> None:
+        self.wcr_server_host = wcr_server_host
+        self.wcr_server_port = wcr_server_port
+        self.wcr_server_protocol = wcr_server_protocol
+        self.base_wz_code_path = base_wz_code_path
+        self.wcr_caller_retry_num = wcr_caller_retry_num
+        self.wcr_caller_timeout = wcr_caller_timeout
+        self.wcr_caller_backoff = wcr_caller_backoff
         self.avatar_server_host = avatar_server_host
         self.avatar_server_protocol = avatar_server_protocol
         self.avatar_server_port = avatar_server_port
@@ -29,6 +43,13 @@ class Config:
 
     def to_json(self) -> dict:
         return {
+            "wcr_server_host": self.wcr_server_host,
+            "wcr_server_port": self.wcr_server_port,
+            "wcr_server_protocol": self.wcr_server_protocol,
+            "base_wz_code_path": self.base_wz_code_path,
+            "wcr_caller_retry_num": self.wcr_caller_retry_num,
+            "wcr_caller_timeout": self.wcr_caller_timeout,
+            "wcr_caller_backoff": self.wcr_caller_backoff,
             "avatar_server_host": self.avatar_server_host,
             "avatar_server_protocol": self.avatar_server_protocol,
             "avatar_server_port": self.avatar_server_port,
