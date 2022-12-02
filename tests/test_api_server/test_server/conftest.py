bf394c7d8a9354a751ac9b7f7a819adefd18fb92
kig9981 <ingu9981@naver.com>
2022-12-02 19:24:29 +0900

    tests: AvatarCallerForTest, InferenceCallerForTest 작성

--- tests/test_api_server/test_server/conftest.py
+++ tests/test_api_server/test_server/conftest.py
@@ -1,6 +1,8 @@
 import logging
 
 import pytest
+import json
+import os
 
 from src.ApiServer.server.config import Config
 from src.ApiServer.server.http_handler import HttpHandler
@@ -8,18 +10,24 @@ from typing import Dict
 
 class AvatarCallerForTest:
     def __init__(self):
-        pass
+        cwd = os.path.dirname(__file__)
+        json_path = os.path.join(cwd, 'test_data', 'test_recommend_data.json')
+        with open(json_path, 'rt') as file:
+            self.data = json.load(file)
 
     async def get_character_look_data(self, encrypted_character_look: str):
-        return None
+        return self.data["get_character_look_data"]
 
     async def get_avatar_image(self, avatar_dict: Dict[str, str]):
-        return None
+        return "avatar image"
 
 
 class InferenceCallerForTest:
     def __init__(self):
-        pass
+        cwd = os.path.dirname(__file__)
+        json_path = os.path.join(cwd, 'test_data', 'test_recommend_data.json')
+        with open(json_path, 'rt') as file:
+            self.data = json.load(file)
 
     async def infer(
         self,
@@ -27,7 +35,7 @@ class InferenceCallerForTest:
         item_parts: str,
         b64_character_look: str,
     ):
-        return None
+        return item_parts
 
 @pytest.fixture
 def test_http_handler(config_for_test: Config):
