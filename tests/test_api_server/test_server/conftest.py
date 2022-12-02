4127d1b970abdb69f48b93681deaa67fc3074063
kig9981 <ingu9981@naver.com>
2022-12-02 18:18:15 +0900

    test: 테스트용 클래스 추가

--- tests/test_api_server/test_server/conftest.py
+++ tests/test_api_server/test_server/conftest.py
@@ -4,7 +4,30 @@ import pytest
 
 from src.ApiServer.server.config import Config
 from src.ApiServer.server.http_handler import HttpHandler
+from typing import Dict
 
+class AvatarCallerForTest:
+    def __init__(self):
+        pass
+
+    async def get_character_look_data(self, encrypted_character_look: str):
+        return None
+
+    async def get_avatar_image(self, avatar_dict: Dict[str, str]):
+        return None
+
+
+class InferenceCallerForTest:
+    def __init__(self):
+        pass
+
+    async def infer(
+        self,
+        gender: str,
+        item_parts: str,
+        b64_character_look: str,
+    ):
+        return None
 
 @pytest.fixture
 def test_http_handler(config_for_test: Config):
@@ -12,5 +35,7 @@ def test_http_handler(config_for_test: Config):
     http_handler = HttpHandler(
         logger=logger,
         config=config_for_test,
+        avatar_caller=AvatarCallerForTest(),
+        inference_caller=InferenceCallerForTest(),
     )
     return http_handler
