e9ca65dc1c0daf9f6e264a6191aa0809727b374d
kig9981 <ingu9981@naver.com>
2022-12-03 19:52:59 +0900

    리뷰사항 수정3

--- src/InferenceServer/__main__.py
+++ src/InferenceServer/__main__.py
@@ -11,13 +11,18 @@ def main():
 
     args = parser.parse_args()
 
+    for gender in gender_list:
+        for part in item_parts:
+            parser.add_argument(f"--{gender}_{part}_model_dir", type=str, help=f"{gender}-{part} model dir", default=f"model/{gender}_{part}/{gender}_{part}_model.pt")
+            parser.add_argument(f"--{gender}_{part}_answer_dict_dir", type=str, help=f"{gender}-{part} answer dict dir", default=f"model/{gender}_{part}/{gender}_{part}_answer_dict.json")
+
     model_list_to_make = json.loads(args.model_json)
 
     model_list = [
         KserveComplementModel(
             name=f"complement-model-{gender}-{part}",
-            model_dir=f"{gender}_{part}_model_dir",
-            model_answer_dict_dir=f"{gender}_{part}_answer_dict_dir",
+            model_dir=f"/mnt/models/complement_model/{gender}_{part}_model.pt",
+            model_answer_dict_dir=f"/mnt/models/complement_model/{gender}_{part}_answer_dict.json",
         ) for gender, part in model_list_to_make
     ]
 
