0c48fcce92b6192c8e2376db50b987223041c023
kig9981 <ingu9981@naver.com>
2022-12-03 17:24:26 +0900

    tests(test_inference_server): test_model.py 작성

---
+++ tests/test_inference_server/test_model.py
@@ -0,0 +1,23 @@
+from src.InferenceServer.Model.model import Model
+import json
+import os
+
+
+def test_request(model_for_test: Model):
+    cwd = os.path.dirname(__file__)
+    json_path = os.path.join(cwd, "testdata", "predict_input.json")
+    with open(json_path) as f:
+        data = json.load(f)
+    json_path = os.path.join(cwd, "testdata", "model_answer_dict_testdata.json")
+    with open(json_path) as f:
+        expected_result = json.load(f)
+    result = model_for_test.predict(request={"instances": data})
+    
+    result = result["predictions"]
+
+    for image_num in range(len(data)):
+        for class_num, (probability, result_item_code) in enumerate(result[image_num]):
+            for index, expected_item_code in expected_result.items():
+                if expected_item_code == result_item_code:
+                    assert index == f"{probability}"
+
