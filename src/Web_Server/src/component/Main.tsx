3e6b9b48487d23605cd2b73bb9599909ff3e6916
Yu KyungHwan <dbrudghks1234@gmail.com>
2022-12-05 02:35:43 +0900

    fix : Main compoent 수정

--- src/Web_Server/src/component/Main.tsx
+++ src/Web_Server/src/component/Main.tsx
@@ -1,23 +1,21 @@
 import React, { useState } from "react";
 import useFetch from "../hooks/useFetch";
-
+import CharacterInfo from "./CharacterInfo";
+import InferImage from "./InferImage";
 import {
-  Box,
-  Card,
-  Typography,
-  CardContent,
-  Paper,
   Grid,
   Button,
   FormGroup,
   TextField,
 } from "@mui/material";
+
 interface FormValues {
   specs?: string;
 }
 
 export default function Main() {
   const [formValues, setFormValues] = useState<FormValues>({});
+  const [devices, setDevices] = React.useState(() => []);
 
   const handleTextFieldChange = (
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -31,87 +29,17 @@ export default function Main() {
   };
 
   let character_code = useFetch("/character_code_web_handler", formValues.UserID);
+  // 추론 관련 API 업데이트 하면 받아 오는 코드
+  // let infer_image = useFetch("/character_code_web_handler", devices);
 
-  const handleSubmit = () => {
+  const characterImageSubmit = () => {
     console.log(character_code.avatar_image)
 
   }
-
   return (
     <Grid container spacing={2}>
       <Grid item xs={6}>
-        <Box
-          sx={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            '& > :not(style)': {
-              m: 1,
-              width: 1250,
-              height: 450,
-            },
-          }}
-        >
-          <Paper elevation={3} >
-          <Box
-          sx={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            '& > :not(style)': {
-              m: 1,
-              width: 150,
-              height: 50,
-            },
-          }}
-        >
-        <Paper elevation={3} >
-        </Paper>
-        <Paper elevation={3} >
-        </Paper>
-        <Paper elevation={3} >
-        </Paper>
-        <Paper elevation={3} >
-        </Paper>
-        </Box>
-
-        <Card sx={{ minWidth: 275 }}>
-          <CardContent>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              <br />
-              <img src = {character_code.avatar_image}/>
-              <br />
-              <br />
-            </Typography>
-            <Typography variant="h5">
-              캐릭터 이미지
-              <br />
-              <br />
-              <Button variant="outlined" >추론하기</Button>
-            </Typography>
-          </CardContent>
-        </Card>
-
-        <Box
-          sx={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            '& > :not(style)': {
-              m: 1,
-              width: 150,
-              height: 50,
-            },
-          }}
-        >
-        <Paper elevation={3} >
-        </Paper>
-        <Paper elevation={3} >
-        </Paper>
-        <Paper elevation={3} >
-        </Paper>
-        <Paper elevation={3} >
-        </Paper>
-        </Box>
-        </Paper>
-        </Box>
+        <CharacterInfo code={character_code} device={devices} />
         <br />
         <FormGroup
           sx={{
@@ -128,65 +56,17 @@ export default function Main() {
             placeholder="UserId를 입력하세요"
             onChange={handleTextFieldChange}
           />
-          <Button variant="outlined" onClick={handleSubmit}>Submit</Button>
+          <Button variant="outlined" onClick={characterImageSubmit}>Submit</Button>
         </FormGroup>
       </Grid>
-
       <Grid item xs={2}>
-        <Box
-          sx={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            '& > :not(style)': {
-              m: 1,
-              width: 1250,
-              height: 300,
-            },
-          }}
-        >
-          <Paper elevation={3}>
-          </Paper>
-          <Paper elevation={3}>
-          </Paper>
-        </Box>
+      <InferImage image_name={[1,4]} code={character_code}/>
       </Grid>
-
       <Grid item xs={2}>
-        <Box
-          sx={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            '& > :not(style)': {
-              m: 1,
-              width: 1250,
-              height: 300,
-            },
-          }}
-        >
-          <Paper elevation={3}>
-          </Paper>
-          <Paper elevation={3}>
-          </Paper>
-        </Box>
+      <InferImage image_name={[2,5]} code={character_code}/>
       </Grid>
-
       <Grid item xs={2}>
-        <Box
-          sx={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            '& > :not(style)': {
-              m: 1,
-              width: 1250,
-              height: 300,
-            },
-          }}
-        >
-          <Paper elevation={3}>
-          </Paper>
-          <Paper elevation={3}>
-          </Paper>
-        </Box>
+      <InferImage image_name={[3,6]} code={character_code}/>
       </Grid>
     </Grid>
   );
