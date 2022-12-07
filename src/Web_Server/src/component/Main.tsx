c10ea1a6852cbd85bbbc5b06cf8840b8eba0ec89
changsu lee <leecs0503@naver.com>
2022-12-08 01:52:18 +0900

    feat(WebServer): snackbar 추가

--- src/Web_Server/src/component/Main.tsx
+++ src/Web_Server/src/component/Main.tsx
@@ -10,6 +10,8 @@ import SendIcon from '@mui/icons-material/Send';
 import {
   Grid,
   Button,
+  Snackbar,
+  Alert,
 } from "@mui/material";
 import InferResultBox from './InferResultBox'
 
@@ -19,10 +21,31 @@ export default function Main() {
     "cryptoUriToRecommand": null,
     "partStateToRecommand": 0,
   });
+  const [snackBar1Open, setSnackBar1Open] = useState(false)
+  const [snackBar2Open, setSnackBar2Open] = useState(false)
   const [partStateToRecommand, setPartStateToRecommand] = useState(0);
   const recommandedInfo = useRecommandFetch(recommandInfo.cryptoUriToRecommand, recommandInfo.partStateToRecommand);
+
+  const snackBar1HandleClose = (event, reason)=>{
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackBar1Open(false)
+  }
+  const snackBar2HandleClose = (event, reason)=>{
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackBar2Open(false)
+  }
+
   const onRecommandButtonClick = (event) => {
     if (characterInfo === null) {
+      setSnackBar1Open(true)
+      return
+    }
+    if (partStateToRecommand === 0) {
+      setSnackBar2Open(true)
       return
     }
     setRecommandInfo({
@@ -33,6 +56,17 @@ export default function Main() {
   console.log(recommandedInfo)
   return (
     <Fragment>
+      <Snackbar open={snackBar1Open} autoHideDuration={6000} onClose={snackBar1HandleClose}>
+        <Alert onClose={snackBar1HandleClose} severity="info" sx={{ width: '100%' }}>
+          유저 아이디를 먼저 입력하세요
+        </Alert>
+      </Snackbar>
+      <Snackbar open={snackBar2Open} autoHideDuration={6000} onClose={snackBar2HandleClose}>
+        <Alert onClose={snackBar2HandleClose} severity="info" sx={{ width: '100%' }}>
+          아무 장비도 선택하지 않았습니다. <br/>
+          변환하고 싶은 장비 아이템을 클릭해서 추천 여부를 토글링 할 수 있습니다.
+        </Alert>
+      </Snackbar>
       <MainIntro />
       <Grid container spacing={2}>
         <Grid item xs={0.25}>
